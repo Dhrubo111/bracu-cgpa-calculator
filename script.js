3b88cf282a27eec6b467539bc603ffc3b88c8ce2
Dhrubo <irfansadidhrubo@gmail.com>
2023-10-31 01:54:08 +0600

    linked services page and show imges of the services

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let homeLink = document.getElementById("home-page");
+  let homeLinkMassage = document.getElementById("home-page-massage");
 
-// testing some code 
-/*
-document.addEventListener("DOMContentLoaded", function () {
-  let tesing = document.getElementById("test");
-let button = document.getElementById("submit");
-  button.addEventListener("click", function () {
-    tesing.textContent = "working";
+  homeLink.addEventListener("click", () => {
+    homeLinkMassage.style.display = "block";
+
+    // Set a timer to hide the message after 3 seconds (you can adjust the time as needed)
+    setTimeout(() => {
+      homeLinkMassage.style.display = "none";
+    }, 3000); // 3000 milliseconds (3 seconds)
   });
 });
-*/
