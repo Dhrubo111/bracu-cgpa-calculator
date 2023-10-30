61b0c7ef5a58a1958da132a1680d51e72599c376
Dhrubo <irfansadidhrubo@gmail.com>
2023-10-31 00:42:11 +0600

    frontend compleat first version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bracu-cgpa-calculator
+
+this is cgpa calculator for bracu students
+
+it is in testing 
---
+++ script.js
@@ -0,0 +1,11 @@
+
+// testing some code 
+/*
+document.addEventListener("DOMContentLoaded", function () {
+  let tesing = document.getElementById("test");
+let button = document.getElementById("submit");
+  button.addEventListener("click", function () {
+    tesing.textContent = "working";
+  });
+});
+*/
